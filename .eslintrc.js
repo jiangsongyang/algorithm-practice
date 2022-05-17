cf7f8b81e317ad38a82dcb75ad46acfddfacdd1b
jiangsongyang <2744856707@qq.com>
2022-05-17 21:42:05 +0800

    [feat] : setup jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    jest:true
   },
   extends: ['standard'],
   parser: '@typescript-eslint/parser',
@@ -9,6 +10,6 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint' ],
   rules: {}
 }
---
+++ src/test.spec.ts
@@ -0,0 +1,3 @@
+test('happy path', () => {
+  expect(1).toBe(1)
+})
