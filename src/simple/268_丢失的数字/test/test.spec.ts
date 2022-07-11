5153c0ec2fff061a84c810994784622418d3c220
jiangsongyang <2744856707@qq.com>
2022-07-11 16:22:39 +0800

    [feat] : update test

--- src/simple/268_丢失的数字/test/test.spec.ts
+++ src/simple/268_丢失的数字/test/test.spec.ts
@@ -2,16 +2,16 @@
 import { missingNumber } from '../src'
 
 describe('happy path', () => {
-  it('base test', () => {
+  it('test [3, 0, 1]', () => {
     expect(missingNumber([3, 0, 1])).toBe(2)
   })
-  it('base test', () => {
+  it('test [0, 1]', () => {
     expect(missingNumber([0, 1])).toBe(2)
   })
-  it('base test', () => {
+  it('test [9, 6, 4, 2, 3, 5, 7, 0, 1]', () => {
     expect(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])).toBe(8)
   })
-  it('base test', () => {
+  it('test [0]', () => {
     expect(missingNumber([0])).toBe(1)
   })
 })
